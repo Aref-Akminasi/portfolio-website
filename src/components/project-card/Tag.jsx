f36b584d0a75db687d1466ddaf2e8898e32ff575
Aref Akminasi <137000185+Aref-Akminasi@users.noreply.github.com>
2023-10-07 13:42:54 +0200

    updated tags rounding

--- src/components/project-card/Tag.jsx
+++ src/components/project-card/Tag.jsx
@@ -1,6 +1,6 @@
 const Tag = (props) => {
   return (
-    <span className="inline-block text-blue px-4 py-2 border rounded-md text-xs mr-3 mt-3">
+    <span className="inline-block text-blue px-4 py-2 border rounded-full text-xs mr-3 mt-3">
       {props.children}
     </span>
   );
