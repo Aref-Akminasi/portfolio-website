819416b721d409f6e8886ad7a20095a4a5a67c34
aref akminasi <aakminasi@quintor.com>
2024-04-26 13:42:40 +0200

    one shadow for project card and aboutme card

--- src/components/about-me/AboutMeCard.jsx
+++ src/components/about-me/AboutMeCard.jsx
@@ -1,6 +1,6 @@
 const AboutMeCard = (props) => {
   return (
-    <div className="max-w-sm bg-lightGray p-12 flex flex-col space-y-8 shadow-lg">
+    <div className="max-w-sm bg-lightGray p-12 flex flex-col space-y-8 shadow-lg hover:shadow-xl duration-300">
       {props.icon}
       <h3 className="font-bold text-xl md:text-2xl">{props.title}</h3>
       <p className="font-regular text-base">{props.paragraph}</p>
