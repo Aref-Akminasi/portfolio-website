a8bb0f78498164b72a54c3248e9008d1d4b74d0e
aref akminasi <aakminasi@quintor.com>
2024-04-26 11:16:48 +0200

    added larger shadow on hover for project card

--- src/components/project-card/ProjectCard.jsx
+++ src/components/project-card/ProjectCard.jsx
@@ -1,14 +1,14 @@
-import ButtonPrimary from '../ButtonPrimary';
-import Tag from './Tag';
-import { Link } from 'react-router-dom';
+import ButtonPrimary from "../ButtonPrimary";
+import Tag from "./Tag";
+import { Link } from "react-router-dom";
 
 const ProjectCard = ({ data }) => {
   return (
-    <div className="max-w-sm flex flex-col">
-      <Link to={'/project/' + data.slug.current}>
+    <div className="max-w-sm flex flex-col shadow-lg hover:shadow-xl duration-300">
+      <Link to={"/project/" + data.slug.current}>
         <img src={data.image.asset.url} alt={data.title} />
       </Link>
-      <div className="flex flex-col space-y-4 h-full justify-between shadow-lg p-4 rounded-b-md">
+      <div className="flex flex-col space-y-4 h-full justify-between  p-4 rounded-b-md">
         <div className="flex flex-col space-y-4 items-start">
           <div className="flex flex-wrap content-start">
             {data.tags.map((tag) => {
@@ -17,7 +17,7 @@ const ProjectCard = ({ data }) => {
           </div>
           <Link
             className="font-bold text-2xl md:text-3xl"
-            to={'/project/' + data.slug.current}
+            to={"/project/" + data.slug.current}
           >
             {data.title}
           </Link>
@@ -25,8 +25,8 @@ const ProjectCard = ({ data }) => {
         </div>
         <div className="flex justify-end">
           <ButtonPrimary
-            size={'text-base'}
-            to={'/project/' + data.slug.current}
+            size={"text-base"}
+            to={"/project/" + data.slug.current}
           >
             Read more
           </ButtonPrimary>
