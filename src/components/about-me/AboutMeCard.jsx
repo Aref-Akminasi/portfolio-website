71dddde8215556792f3564fa98de9cfed785dd80
aref akminasi <aakminasi@quintor.com>
2024-04-26 11:34:28 +0200

    edite AboutMeCard shadow

--- src/components/about-me/AboutMeCard.jsx
+++ src/components/about-me/AboutMeCard.jsx
@@ -1,6 +1,6 @@
 const AboutMeCard = (props) => {
   return (
-    <div className="max-w-sm bg-lightGray p-12 flex flex-col space-y-8 ">
+    <div className="max-w-sm bg-lightGray p-12 flex flex-col space-y-8 shadow-md">
       {props.icon}
       <h3 className="font-bold text-xl md:text-2xl">{props.title}</h3>
       <p className="font-regular text-base">{props.paragraph}</p>
